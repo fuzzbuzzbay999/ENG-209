3c3b696def42d55773616f069275ea3fa1a87483
fuzzbuzzbay999 <fuzzbuzzbay999@gmail.com>
2025-08-22 12:32:09 -0700

    I forgot to annotate the code

--- Juliano_Declan_ENG209_HW0.m
+++ Juliano_Declan_ENG209_HW0.m
@@ -1,7 +1,12 @@
+%initialize variables
 a=0;
 b=0;
+
+%make a function that finds the hypotenuse from two lengths, in the form of findlen(float,float)
 function findlen(a,b)
+  %calculate the hypotenuse and output its return
   sqrt(a^2+b^2)
 end
 
+%call findlen
 findlen(12,12);
