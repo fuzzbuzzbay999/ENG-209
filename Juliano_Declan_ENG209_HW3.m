aedd6615c2a62c9d0d9c9a864f1e99897838f3a9
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-09-09 16:30:42 -0700

    worked on hw 3

--- Juliano_Declan_ENG209_HW3.m
+++ Juliano_Declan_ENG209_HW3.m
@@ -28,7 +28,7 @@ p1_x = [-2:0.1:0];
 p1 = p1_x+4;
 
 p2_x = [0:0.1:1];
-p2 = 4-4*p2_x;
+p2 = 4-p2_x.^2;
 
 p3_x = [1:0.1:2];
 p3 = 3*p3_x.^2;
@@ -41,3 +41,18 @@ plot(p1_x,p1,'m')
 plot (p2_x,p2,'m')
 plot(p3_x,p3,'m')
 hold off
+
+    %question 7
+%{
+compute and graph the weight (in newtons, or N) of a 100 kg object at various altitudes
+above the earth’s surface, as a function of altitude, or distance from the earth’s surface (in
+kilometers, or km). altitude (km).
+%}
+t=[0:0.1:10];
+alt=t;
+G = 6.674e-11;              %gravitational constant (m3/kg/s2)
+M1 = 5.972e24;              %mass of Earth (kg)
+M2 = 100;                   %mass of of object (kg)
+R = 6371+alt*200;               %distance between Earth and object (m)
+F = (G * (M1 * M2)) ./ R.^2;  %calculate weight (N)
+plot(t,F,t,alt)
--- class_work.m
+++ class_work.m
@@ -1,13 +1,27 @@
-b="sdfadss"
-fprintf(' You owe %1.2f \n please check your bill. \n',b)
-filetoprint=rand(5,3)
-sales = load ("AutoSalesUS_2001_2022.txt");
-years = sales(:,1)
-amount = sales(:,2)
-plot(years,amount)
-grid on
-legend
-colorbar
-title("car sales v years")
-xlabel("years")
-ylabel("amount")
+p1_x = [-3:0.1:0];
+p1 = p1_x+1;
+
+p2_x = [0:0.1:2];
+p2 = p2_x*0+1;
+
+p3_x = [2:0.1:3];
+p3 = p3_x.^2-3;
+subplot(2,2,1)
+hold on
+grid('on')
+title("Piecewise function f(x)")
+xlabel("x")
+ylabel("y")
+plot(p1_x,p1,'m')
+plot (p2_x,p2,'m')
+plot(p3_x,p3,'m')
+hold off
+subplot(2,5,3)
+t=[0:0.1:10];
+alt=t;
+G = 6.674e-11;              %gravitational constant (m3/kg/s2)
+M1 = 5.972e24;              %mass of Earth (kg)
+M2 = 100;                   %mass of of object (kg)
+R = 6371+alt*200;               %distance between Earth and object (m)
+F = (G * (M1 * M2)) ./ R.^2;  %calculate weight (N)
+plot(t,F,t,alt)
