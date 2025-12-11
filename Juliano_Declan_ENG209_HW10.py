ccbc01cbbfd6ac91868e1837b9a315496e078fab
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2025-12-11 00:43:04 -0700

    hw 10

---
+++ Juliano_Declan_ENG209_HW10.py
@@ -0,0 +1,121 @@
+
+# Juliano_Declan_ENG209_HW10.py
+#12/9/2025
+#ENG 209, HW 10, exercises:
+
+# import all of math
+from math import *
+import numpy as n               # numpy for arrays and math functions
+import matplotlib.pyplot as plt # matplotlib for plotting
+import matplotlib.ticker as tck # matplotlib ticker for setting tick marks
+
+#***************************************question 1********************************
+'''
+Plot 𝑦 = cos𝑥 for x from 0 to 10π. 
+'''
+# x values from 0 to 10π, 200 points
+x  = n.linspace(0,10*pi,200)
+
+y = n.cos(x)    # y values, cosine of x
+
+# create figure and plot
+fig = plt.figure(1)
+ax = fig.add_subplot()
+
+# plot cosine function, set color, line width, and line style
+ax.plot(x,y,color = 'green',linewidth = '3',linestyle  = 'dashed')
+ax.yaxis.set_major_locator(tck.MultipleLocator(0.5))    # Major ticks every 0.5 # set_yticks(n.arange()) is a little clunky
+ax.yaxis.set_minor_locator(tck.MultipleLocator(0.1))    # Minor ticks every 0.1 # set_yticks(n.arange(),minor = 'true') is a little clunky
+
+# set title, labels, and grid
+ax.set_title(r'$f(x) = \cos(x)$')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.grid()
+plt.show(block = False)     # show plot
+
+#***************************************question 3********************************
+'''
+make a 2x2 grid of graphs of the folowing functions for 0 < x < 4pi
+𝑦1 = sin𝑥 
+𝑦2 = sin2 𝑥 
+𝑦3 = 𝑥sin𝑥 
+𝑦4 = sin𝑥/x
+
+'''
+# x values from 0 to 4π, 200 points
+x = n.linspace(0,4*pi,200)
+
+# y values for each function
+y1 = n.sin(x)
+y2 = n.sin(x)**2
+y3 = x * n.sin(x)
+y4 = n.sin(x)/x
+
+# create figure 2 and subplots, graph and set titles, for each subplot
+fig2 = plt.figure(2,figsize=(10,8)) 
+fig2.suptitle('Multiple graphs of sin')
+
+bx = fig2.add_subplot(2,2,1)    # subplot 1
+bx.plot(x,y1,color = 'yellow', linewidth = '2.5')
+bx.set_title(r'$f(x) = \sin(x)$')
+
+bx = fig2.add_subplot(2,2,2)    # subplot 2
+bx.plot(x,y2,color = 'yellow', linewidth = '2.5')
+bx.set_title(r'$f(x) = \sin(x)^2$')
+
+bx = fig2.add_subplot(2,2,3)    # subplot 3
+bx.plot(x,y3,color = 'yellow', linewidth = '2.5')
+bx.set_title(r'$f(x) = x * \sin(x)$')
+
+bx = fig2.add_subplot(2,2,4)    # subplot 4
+bx.plot(x,y4,color = 'yellow', linewidth = '2.5')
+bx.set_title(r'$f(x) = \frac{\sin(x)}{x}$')
+
+#*********************************question 5********************************
+'''
+Plot the gravitational force between a 90kg object and the earth as a function of distance from the earth's surface.
+The distance should range from 0 to 1000km, with a step size of 10km.
+'''
+# constants
+G = 6.674E-11   # gravitational constant
+r = 6371 * 1000 # radius of earth in meters
+m1 = 5.972E24   # mass of earth kg
+
+# modifiables
+m2 = 90     # mass kg
+a = 0       # start km
+b = 1000    # end km
+d = 10      # distance km
+
+r2 = n.arange(a,b,d,dtype='float64')  # distance from earth surface in km force 64 bit numbers because numpy and python are absolute pieces of garbage
+
+
+# function to calculate gravitational force at a given distance from the earth's surface
+def grav(R):
+    R = R *1000                     # *= doesnt work reliably in numpy (transfers types so if int32 it will remain int32 regardless if the multiple ie 1000.3)
+    F = (G * m1 * m2)/((r+R)**2)    # force in newtons
+    return F
+
+fig3 = plt.figure(3) # create figure 3
+cx = fig3.add_subplot()
+
+# plot force vs distance from earth surface and label axes, add grid, tick marks, and title
+cx.plot(r2,grav(r2)) 
+cx.set_xlabel('Distance from earth surface (km)')
+cx.set_ylabel('Gravitational force orce (N)')
+cx.grid()
+cx.xaxis.set_major_locator(tck.MultipleLocator(100))
+cx.set_title('Gravitational Force vs Distance from Earth Surface')
+    
+# Arrows showing the altitudes where each of the following atmospheric layers begin: stratosphere, mesosphere, thermosphere, exosphere with astronaut
+s = 12      # stratosphere starts at 12 km
+m = 50      # mesosphere starts at 50 km
+t = 80      # thermosphere starts at 80 km
+ex = 700    # exosphere starts at 700 km
+cx.annotate('Stratosphere (John Young)',xy = (s,grav(s)), xytext = (s+25,grav(s)), arrowprops=dict(arrowstyle='->',color = 'red'),fontsize = '12')
+cx.annotate('Mesosphere (Sally Ride)',xy = (m,grav(m)), xytext = (m+100,grav(m)-20), arrowprops=dict(arrowstyle='->',color = 'red'),fontsize = '12')
+cx.annotate('Thermosphere (Mae Jemison)',xy = (t,grav(t)), xytext = (t,grav(t)-150), arrowprops=dict(arrowstyle='->',color = 'red'),fontsize = '12')
+cx.annotate('Exosphere (Guion Bluford)',xy = (ex,grav(ex)), xytext = (ex-150,grav(ex)+100), arrowprops=dict(arrowstyle='->',color = 'red'),fontsize = '12')
+
+plt.show() #keep the plots open
--- python _test.py
+++ python _test.py
@@ -1,14 +1,40 @@
-from math import *
-r = list(range(5,55,5))
-def surf_area(r):
-    area = 4*pi*(r**2)
-    return area
+from mpl_toolkits.mplot3d import *
+import  matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
 
-def volume(r):
-    vol = 4/3 * pi * r ** 3
-    return vol
-print(f'{"r":>5}\t{"area":>10}\t{"vol":>10}')
-print('-'*38)
-for i in r:
-    print(f'{i:>5}\t{surf_area(i):>10.3f}\t{volume(i):>10.3f}')
-    
+from math import *
+x = np.linspace(-2,2,400)
+y=x.copy()
+X,Y = np.meshgrid(x,y)
+Z = np.exp(-(X**2+Y**2))
+
+fig = plt.figure()
+ax = fig.add_subplot(projection = '3d')
+ax.plot_surface(X,Y,Z,rstride = 10, cstride = 30, cmap='rainbow')
+print(ax.get_zticks())
+
+# f(x) = x lnx
+# f'(x) = ln(x)+1
+a = 1; b = 2; h = 0.1
+n = int(round((b-a)/h)) + 1
+x = np.linspace(a,b,n)
+xdir = x[0:len(x)-1]
+y = x * np.log(x)
+yext = 1+np.log(x)
+yest = (y[1:n]-y[0:n-1])/h
+fig = plt.figure()
+fx = fig.add_subplot()
+fx.plot(x,yext,label = 'exact deriv')
+fx.plot(xdir,yest,label = 'estimated deriv')
+fx.legend()
+
+a = 0; b = 6; h = 0.01
+n = int(round((b-a)/h)) + 1
+x = np.linspace(a,b,n)
+y = 0.25 * x ** 2
+
+int_est = h*np.sum(y[1::])
+print(int_est)
+plt.show()
+print(sp.integrate.quad(sin,0,pi))
